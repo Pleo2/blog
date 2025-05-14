77074c17ea8d50c076f4673cd9ee87563f8a37c5
pleo2 <leooel23m@gmail.com>
2025-05-13 20:28:24 -0400

    refactor(bento): enhance text responsiveness and layout in expertise, repository, and Wakatime hours cards

--- components/bento/container-1/expertise-card.tsx
+++ components/bento/container-1/expertise-card.tsx
@@ -11,13 +11,21 @@ export const ExpertiseCard = () => {
             )}
         >
             <div className="flex flex-col justify-center items-center gap-2 hover:scale-105 transition-scale duration-400">
-                <h2 className="text-5xl sm:text-5xl text-transparent bg-gradient-to-b from-gray-100/90 via-gray-300/50 bg-clip-text flex justify-center items-center">
+                <h2 className="text-5xl sm:text-5xl lg:text-4xl xl:text-5xl text-transparent bg-gradient-to-b from-gray-100/90 via-gray-300/50 bg-clip-text flex justify-center items-center">
                     03{" "}
-                    <span className="text-white/80 text-4xl sm:text-4xl md:text-5xl">
+                    <span className="text-white/80 text-4xl sm:text-4xl md:text-5xl lg:text-2xl xl:text-5xl ">
                         +
                     </span>
                 </h2>
-                <p className="gap-1 flex text-[10px] sm:text-xs items-center border border-white/20 p-1 sm:p-2 rounded-lg text-white/80 w-28 sm:w-32 md:w-36 justify-center whitespace-nowrap overflow-hidden text-ellipsis">
+                <p
+                    className={cn(
+                        "gap-1 flex text-[10px] rounded-lg text-white/80 w-28 items-center border border-white/20 p-1 justify-center whitespace-nowrap overflow-hidden text-ellipsis",
+                        "sm:p-2 sm:text-xs sm:w-32 ",
+                        "md:w-36",
+                        "lg:text-[10px] lg:px-1 lg:w-max",
+                        "xl:p-2 text-xs w-32"
+                    )}
+                >
                     <span>
                         <Star className="w-3 sm:w-4" />
                     </span>
--- components/bento/container-1/repository-card.tsx
+++ components/bento/container-1/repository-card.tsx
@@ -15,13 +15,39 @@ export const RepositorioCard = ({
             )}
         >
             <div className="flex flex-col justify-center items-center gap-2 hover:scale-105 transition-scale duration-400">
-                <h2 className="text-5xl sm:text-5xl  text-transparent bg-gradient-to-b from-gray-100/90 via-gray-300/50 bg-clip-text flex justify-center items-center">
+                <h2
+                    className={cn(
+                        "text-transparent bg-gradient-to-b from-gray-100/90 via-gray-300/50 bg-clip-text flex justify-center items-center",
+                        "text-5xl",
+                        "sm:text-5xl",
+                        "lg:text-4xl",
+                        "xl:text-5xl"
+                    )}
+                >
                     {numberRepositories}
-                    <span className="text-white/80 text-4xl sm:text-4xl md:text-5xl">
+                    <span
+                        className={cn(
+                            "text-white/80",
+                            "text-4xl",
+                            "sm:text-4xl",
+                            "md:text-5xl",
+                            "lg:text-2xl",
+                            "xl:text-5xl"
+                        )}
+                    >
                         +
                     </span>
                 </h2>
-                <p className="gap-1 flex text-[10px] sm:text-xs items-center border border-white/20 p-1 sm:p-2 rounded-lg text-white/80 w-28 sm:w-32 md:w-36 justify-center whitespace-nowrap overflow-hidden text-ellipsis">
+                <p
+                    className={cn(
+                        "gap-1 flex items-center border border-white/20 rounded-lg text-white/80 justify-center whitespace-nowrap overflow-hidden text-ellipsis",
+                        "text-[10px] p-1 w-28",
+                        "sm:text-xs sm:p-2 sm:w-32",
+                        "md:w-36",
+                        "lg:text-[10px] lg:px-1 lg:w-max",
+                        "xl:text-xs p-2 w-36"
+                    )}
+                >
                     <span>
                         <Github className="w-3 sm:w-4" />
                     </span>
