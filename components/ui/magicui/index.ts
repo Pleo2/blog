697ba94b90d201366e9f95b8a652e531e3bae942
pleo2 <leooel23m@gmail.com>
2025-08-02 03:05:33 -0400

    chore: create barrel file for magicui exports

---
+++ components/ui/magicui/index.ts
@@ -0,0 +1,8 @@
+export { default as AnimatedListShow } from "./animated-list-show";
+export { AnimatedList } from "./animated-list";
+export { BentoGrid } from "./bento-grid";
+export { BorderBeam } from "./border-beam";
+export { Marquee } from "./marquee";
+export { Meteors } from "./meteors";
+export { NumberTicker } from "./number-ticker";
+export { ShineBorder } from "./shine-border";
